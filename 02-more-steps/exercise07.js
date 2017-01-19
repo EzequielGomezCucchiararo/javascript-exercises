1f9f5eb40189fdfc2918595c61c29cdcbd91e745
Ezequiel Gomez <ezequiel.cucchiararo@gmail.com>
2017-01-19 12:15:27 +0100

    exercise resolved

--- 02-more-steps/exercise07.js
+++ 02-more-steps/exercise07.js
@@ -5,8 +5,17 @@
 // Javascript object. Try it with something like
 // charFreq("abbabcbdbabdbdbabababcbcbab")
 
-function charFreq () {
-
+function charFreq (text) {
+  var charFreqObject = {}
+  var textLeng = text.length
+  for (var i = 0; i < textLeng; i++) {
+    if (!charFreqObject[text[i]]) {
+      charFreqObject[text[i]] = 1
+    } else {
+      charFreqObject[text[i]] += 1
+    }
+  }
+  return charFreqObject
 }
 
-charFreq('abbabcbdbabdbdbabababcbcbab')
+charFreq('abbabcbdbabdbdbabababcbcbabe')
